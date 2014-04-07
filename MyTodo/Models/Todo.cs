a44e2e68643b80ab1a251c17956290348e639cdc
Nicholas Allen <nicholas.allen88@gmail.com>
2014-04-07 17:37:15 +0100

    Adding my inital ASP.NET Todo list project to the repo

---
+++ MyTodo/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MyTodo.Models {
+    public class Todo {
+        public int ID { get; set; }
+        public bool Completed { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class TodoDbContext : DbContext {
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
